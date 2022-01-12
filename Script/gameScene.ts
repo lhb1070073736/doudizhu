793712d78418b1c6ccd43db1f21621f3517853f3
lhb1070073736 <1070073736@qq.com>
2022-01-12 14:46:30 +0800

    11

---
+++ Script/gameScene.ts
@@ -0,0 +1,25 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+        cc.find("Canvas/GameScene/button/youxi_fanhui").on(cc.Node.EventType.TOUCH_START,function(){
+            cc.director.loadScene("SceneOne");
+        })
+    }
+
+    // update (dt) {}
+}
